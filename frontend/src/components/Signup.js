7baa762e35dc616efd9b6aa2c46b226ab4ed4ede
Sourabh Bansotiya <sbansotiya@gmail.com>
2025-09-14 15:25:42 +0530

    change signup import

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-import api from '../api/axiosConfig';
+import api from "../api/axiosConfig";
 
 export default function Signup() {
 
